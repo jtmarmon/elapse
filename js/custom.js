0233e766100f2ec8b613e2321211740ea3b92edf
Jason Marmon <jtmarmon@gmail.com>
2014-04-03 21:35:19 -0400

    workin

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+/*
+
+<label for=fader>Volume</label>
+<input type=range min=0 max=100 value=50 id=fader step=1 onchange="outputUpdate(value)">
+<output for=fader id=volume>50</output>
+<script>
+function outputUpdate(vol) {
+document.querySelector('#volume').value = vol;
+}
+</script>
+*/
