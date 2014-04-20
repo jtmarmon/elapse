dd3d21cf038ba3467f68e9f71eea3c495ee7b346
Jason Marmon <jtmarmon@gmail.com>
2014-04-20 12:23:31 -0400

    switched to angularJS

---
+++ js/modules.js
@@ -0,0 +1,67 @@
+var elapse = angular.module("elapse", ['ngRoute']);
+elapse.config(function($routeProvider)
+{
+	$routeProvider
+	.when('/',
+	{
+		controller:'ScheduleController',
+		templateUrl: 'schedule.html'
+	})
+	.when('/timers',
+	{
+		controller:'TimerController',
+		templateUrl:'timers.html'
+	})
+	.otherwise({redirectTo: '/'});
+});
+var instances = 0;
+elapse.controller('ScheduleController', function($scope){
+
+	$scope.events = []
+	function initPicker(picker)
+		{
+			$(picker).ColorPicker(
+				({
+						
+						onShow: function (colpkr) {
+							$(colpkr).fadeIn(500);
+							//need to simulate click here //what....for some reason this is an undefined function but still calls the select method WTF
+							return false;
+						},
+						onHide: function (colpkr) {
+							$(colpkr).fadeOut(500);
+							return false;
+						},
+						onChange: function (hsb, hex, rgb) {
+							$(picker).css('backgroundColor', '#' + hex);
+							//picker4
+							var pickerIndex = $(picker).attr('id').substring(6, $(picker).attr('id').length);
+							console.log(pickerIndex);
+							$scope.events[pickerIndex].color = "#" + hex; 
+							$scope.$apply();
+						}
+					})
+				);
+		}
+	$scope.addScheduleElement = function()
+	{
+		var newEvent = {title:"", color:"#FFF000",duration:"30 minutes", index:instances++}
+		$scope.events.push(newEvent);
+		$(".preview-circle").each(function(index,ele)
+		{
+			console.log(index);
+			initPicker(ele);
+		});
+
+	}
+	$(document).ready(function(){
+		console.log("sadasdadaa");
+		$scope.addScheduleElement();
+	});
+	
+	function randomColor()
+	{
+		return '#'+Math.floor(Math.random()*16777215).toString(16);
+	}
+	
+});
