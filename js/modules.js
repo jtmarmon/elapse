ebd97534e6ec2e5331d1388ae3269823df9f181f
Jason Marmon <jtmarmon@gmail.com>
2014-04-21 22:51:40 -0400

    i don't remember everything i changed so just shut it git

--- js/modules.js
+++ js/modules.js
@@ -5,12 +5,17 @@ elapse.config(function($routeProvider)
 	.when('/',
 	{
 		controller:'ScheduleController',
-		templateUrl: 'schedule.html'
+		templateUrl: 'views/schedule.html'
 	})
 	.when('/timers',
 	{
 		controller:'TimerController',
-		templateUrl:'timers.html'
+		templateUrl:'views/timers.html'
+	})
+	.when('/end',
+	{
+		controller:'EndController',
+		templateUrl:'views/end.html'
 	})
 	.otherwise({redirectTo: '/'});
 });
@@ -90,14 +95,65 @@ elapse.controller('ScheduleController', function($scope, $timeout, $location, sc
 	
 });
 //TIMER CONTROLLER
+/*
+	1) Use total duration of event to determine intervals
 
+*/
 
 elapse.controller('TimerController', function($scope, $timeout, $location, schedule){
 	$scope.events = schedule.getData();
-	$scope.currentEvent = $scope.events[0];
+	var currentEventIndex = 0;
+	$scope.currentEvent = $scope.events[currentEventIndex];
 	$scope.currentStyle = {
 		background:$scope.currentEvent.color
 	}
-	console.log("execuer");
+	$scope.timer = function()
+	{
+		var start = new Date().getTime(),
+		elapsed = '0.0';
+		var seconds, minutes= 0;
+		var storedHours = $scope.currentEvent.duration.hours;
+		var storedMinutes = $scope.currentEvent.duration.minutes;
+		var interval = window.setInterval(function()
+		{
+			if($scope.currentEvent.duration.hours == 0 && $scope.currentEvent.duration.minutes == 0)
+			{
+				window.clearInterval(interval);
+				$scope.nextEvent();
+				seconds,minutes = 0;
+				elapsed = '0.0';
+				start = new Date().getTime();
+			}
+	 	   var time = new Date().getTime() - start;
+		    elapsed = Math.floor(time / 100) / 10;
+		    if(Math.round(elapsed) == elapsed) { elapsed += '.0'; }
+			    document.title = elapsed;
+			    seconds = elapsed;
+			    minutes = seconds/1;
+			    $scope.currentEvent.duration.minutes = storedMinutes - Math.floor(minutes);
+			    $scope.$apply();
+				}, 100);
+	}
+	$scope.nextEvent = function()
+	{
+		currentEventIndex++;
+		if(currentEventIndex < $scope.events.length)
+			$scope.currentEvent = $scope.events[currentEventIndex];
+		else
+			$location.path('end');
+		console.log("CURRENT EVENT:::::: " + $scope.currentEvent);
+		$scope.currentStyle = {
+		background:$scope.currentEvent.color
+	}
+		console.log($scope.currentEvent.color);
+		$scope.timer();
+	}
+	$(document).ready(function(){
+		$scope.timer();
+	});
+});
 
+
+elapse.controller('EndController', function($scope, $timeout, $location, schedule){
+		
 });
